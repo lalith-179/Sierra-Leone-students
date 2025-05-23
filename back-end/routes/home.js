e8df7e8c5a7887005409235c119dbf95ec021d2a
Mangala Lalitendra <97622517+lalith-179@users.noreply.github.com>
2025-05-23 19:00:15 +0530

    Delete back-end directory

--- back-end/routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-    res.render("index", { title: "SLSU_AP", message: "Welcome to SLSU_AP" });
-  });
-
-module.exports = router;
